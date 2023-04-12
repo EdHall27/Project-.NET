e3b282e7cb71aded2ff61e52e02890dc2e126ef2
EdHall <halluchroger@gmail.com>
2023-04-12 12:42:52 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/NotFoudException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoudException : ApplicationException
+    {
+        public NotFoudException(string message) : base(message)
+        {
+
+        }
+    }
+}
